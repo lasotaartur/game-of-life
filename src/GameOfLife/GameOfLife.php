88ce7679bde6ad66c9088d26c03be0b72e1ef60c
Artur Lasota <artur.lasota@performgroup.com>
2018-09-28 14:07:33 +0200

    init

--- src/GameOfLife/GameOfLife.php
+++ src/GameOfLife/GameOfLife.php
@@ -13,10 +13,56 @@ final class GameOfLife
             system('clear');
             print $input;
             print "Printing timestamp just so you know the script is running " . time();
+            $input = $this->transform($input);
         }
     }
 
-    public function test(): string {
-        return 'not working';
+    public function transform(string $input): string
+    {
+        $newData = [];
+        $data = array_map(function ($row) {
+            return str_split($row);
+        }, explode("\n", trim($input)));
+        $height = count($data);
+        $width = count($data[0]);
+        for ($x = 0; $x < $width; $x++) {
+            for ($y = 0; $y < $height; $y++) {
+                $newData[$y][$x] = $this->getNextState(
+                    $data[$y][$x],
+                    $this->getNumberOfActiveCells($this->getNeighborhood($y, $x, $height, $width), $data)
+                );
+            }
+        }
+        return implode("\n", array_map(function ($row) {
+                return implode('', $row);
+            }, $newData)) . "\n";
+    }
+
+    public function getNeighborhood(int $x, int $y, int $width, int $height): array
+    {
+        return array_reduce([[0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]], function ($carry, $offset) use ($x, $y, $width, $height) {
+            $nx = $x + $offset[0];
+            $ny = $y + $offset[1];
+            if ($nx > -1 && $nx < $width && $ny > -1 && $ny < $height) {
+                return array_merge($carry, [[$nx, $ny]]);
+            }
+            return $carry;
+        }, []);
+    }
+
+    public function getNumberOfActiveCells(array $corrds, array $data): int
+    {
+        return array_reduce($corrds, function ($carry, $corrd) use ($data) {
+            return $carry + ($data[$corrd[0]][$corrd[1]] === 'x' ? 1 : 0);
+        }, 0);
+
+    }
+
+    public function getNextState(string $currentState, int $numberOfAliveNeighbors): string
+    {
+        if ($currentState === '.') {
+            return $numberOfAliveNeighbors === 3 ? 'x' : '.';
+        }
+        return in_array($numberOfAliveNeighbors, [2, 3]) ? 'x' : '.';
     }
 }
--- tests/GameOfLife/GameOfLifeTest.php
+++ tests/GameOfLife/GameOfLifeTest.php
@@ -11,16 +11,44 @@ final class GameOfLifeTest extends TestCase
     /**
      * @var GameOfLife
      */
-    private $vendingMachine;
+    private $gameOfLife;
 
     public function setUp(): void
     {
-        $vm = new GameOfLife();
-        $this->vendingMachine = $vm;
+        $this->gameOfLife = new GameOfLife();
     }
 
-    public function testServiceReturnsNoService(): void
+    public function testGetNeighborhood(): void
     {
-        $this->assertEquals('working', $this->vendingMachine->test());
+        $this->assertEquals([[1, 0], [1, 1], [0, 1]], $this->gameOfLife->getNeighborhood(0, 0, 3, 3));
+        $this->assertEquals([[2, 0], [2, 1], [1, 1], [0, 1], [0, 0]], $this->gameOfLife->getNeighborhood(1, 0, 3, 3));
+        $this->assertEquals([[1, 0], [2, 0], [2, 1], [2, 2], [1, 2], [0, 2], [0, 1], [0, 0]], $this->gameOfLife->getNeighborhood(1, 1, 3, 3));
+    }
+
+    public function testGetNumberOfAliveCell(): void
+    {
+        $data = [
+            ['.', 'x', '.'],
+            ['x', '.', '.'],
+            ['.', '.', 'x']
+        ];
+        $tests = [
+            [[[0, 0], [1, 0], [0, 1]], 2],
+            [[[0, 0], [1, 0], [2, 0], [2, 1], [2, 2], [1, 2], [0, 2], [0, 1]], 3],
+            [[[0, 0], [2, 0], [1, 1], [2, 1], [0, 2], [1, 2]], 0]
+        ];
+
+        foreach ($tests as $test) {
+            $this->assertEquals($test[1], $this->gameOfLife->getNumberOfActiveCells($test[0], $data));
+        }
+    }
+
+    public function testNextState(): void
+    {
+        $this->assertEquals('x', $this->gameOfLife->getNextState('.', 3));
+        $this->assertEquals('.', $this->gameOfLife->getNextState('.', 4));
+        $this->assertEquals('x', $this->gameOfLife->getNextState('x', 2));
+        $this->assertEquals('x', $this->gameOfLife->getNextState('x', 3));
+        $this->assertEquals('.', $this->gameOfLife->getNextState('x', 4));
     }
 }
